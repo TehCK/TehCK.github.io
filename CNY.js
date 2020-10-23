d1b40d2a34ae748d4127f0c8c8d259a24d24b515
TehCK <tehck0922@gmail.com>
2020-10-23 10:59:14 +0800

    Update CNY.js

--- CNY.js
+++ CNY.js
@@ -1,12 +1,3 @@
 function whatsapp() {
 	window.location = 'http://wa.me/?text=https://tehck.github.io/individual_assignment_CNY1.html'
 }
-
-function loadname() {
-	var mytext = getUrlVars()["text"];
-	var mytext2 = mytext.replace("%20", "&nbsp");
-	var mytext3 = mytext2.replace("%20", "&nbsp");
-	var mytext4 = mytext3.replace("%20", "&nbsp");
-	var mytext5 = mytext4.replace("%20", "&nbsp");
-	document.write("<p class='aligncenter'><b><font size='600' face='verdana' color='yellow'> " + mytext5.big() + "</font></b></p>");
-}
