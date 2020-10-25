672dc582d24011f4b259b7b317294ea6ce32b58a
TehCK <tehck0922@gmail.com>
2020-10-25 12:51:48 +0800

    Update Chris.js

--- Chris.js
+++ Chris.js
@@ -1,5 +1,5 @@
 function link() {
-	window.location = 'file:///C:/Users/umaru/Documents/GitHub/My_Website/TehCK.github.io/Chris2.html'
+	window.location = 'https://tehck.github.io/Chris2.html'
 }
 
 function whatsapp() {
